cc257c529c3a30062e7d87a84b5a60012fb8d5a8
=Romina Dianat <=>
2021-12-30 19:36:08 +0330

    first commit

---
+++ game.py
@@ -0,0 +1,15 @@
+import random
+answer = random.randint(1 , 99)
+
+guess = input(" what is your guess? ")
+guess = int(guess)
+
+while guess != answer:
+    if guess > answer:
+        print("my number is smaller!")
+    else:
+        print("my number is larger!")
+    guess = input(" what is your guess? ")
+    guess = int(guess)
+
+print("wow you did it!!!!")
